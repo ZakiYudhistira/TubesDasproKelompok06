35b0a905fda446594c7c56b280a6ef2fffee7aac
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2023-04-08 21:52:44 +0700

    coba fungsi login

---
+++ Sub-program/LoginLogout/fungsi_login.py
@@ -0,0 +1,18 @@
+import csv
+data_user_read = open('user.csv','r')
+data_user_reader = csv.reader(data_user_read,delimiter=';')
+
+def login():
+    akses = False
+    while akses == False:
+        user = input("Masukkan username : ")
+        password = input("Masukkan password : ")
+        for row in data_user_reader:
+            cek_user = (row[0],row[1])
+            if cek_user == (user,password):
+                akses = True
+        if akses == False:
+            print("Username dan password tidak sesuai atau username tidak terdaftar, mohon coba lagi")
+        elif akses:
+            print("Login berhasil")
+login()
