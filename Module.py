0b5485b36d354eec6a4ca4b4350155a7f688d485
Irfan Nurhakim Hilmi <irfannurhakim5@gmail.com>
2023-04-09 15:24:47 +0700

    Added fungsi load

--- Module.py
+++ Module.py
@@ -34,3 +34,41 @@ def login(): # fungsi login yang akan mengoutput True bila login berhasil dan Fa
 #------------------------------------------------------------------------------------------------------------ Fungsi logout    
 def logout():
     return False, " ", " "
+
+
+
+#------------------------------------------------------------------------------------------------------------ Fungsi load_data
+def load_data(filename):
+    file = open(filename, 'r').read()
+    if filename == "user.csv":
+        NMax = 102
+        NParam = 3
+    elif filename == "candi.csv":
+        NMax = 100
+        NParam = 5
+    else:
+        NMax = 4
+        NParam = 3
+
+    matriks_data = [[0 for i in range(NParam)] for j in range(NMax)]
+    data = ''
+    indeks_baris = 0
+    indeks_kolom = 0
+
+    for huruf in file:
+        if huruf == ';' or huruf == '\n':
+            if huruf != '\n':
+                matriks_data[indeks_baris][indeks_kolom] = data
+                indeks_kolom += 1
+            else:
+                matriks_data[indeks_baris][indeks_kolom] = data
+                indeks_baris += 1
+                indeks_kolom = 0
+            data = ''
+        else:
+            data += huruf
+
+    if data != '':
+        matriks_data[indeks_baris][indeks_kolom] = data
+
+    return matriks_data
